4bcf47c459822402db4374252b31439ffe6f257d
juanmager <juanmadaria@hotmail.es>
2020-08-12 01:17:46 -0300

    First upload

---
+++ exercise_1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+if a < b:
+    print("The biggest number is: " ,a)
+
+elif a == b:
+    print( "The number " , a , " and the number" , b , " they are equal")
+    
+else:
+    print("The smallest number is: " ,b)
