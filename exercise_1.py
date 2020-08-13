5cacd90aea4a0a26a2b2f81e5ccb2bc87c012d4f
juanmager <juanmadaria@hotmail.es>
2020-08-13 01:11:19 -0300

    New exercises uploaded

--- exercise_1.py
+++ exercise_1.py
@@ -1,11 +1,41 @@
-a = int(input("Enter a number: "))
-b = int(input("Enter another number: "))
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
 
-if a < b:
-    print("The biggest number is: " ,a)
+# if a < b:
+#     print("The biggest number is: " ,a)
 
-elif a == b:
-    print( "The number " , a , " and the number" , b , " they are equal")
+# elif a == b:
+#     print( "The number " , a , " and the number" , b , " they are equal")
     
-else:
-    print("The smallest number is: " ,b)
+# else:
+#     print("The smallest number is: " ,b)
+
+# name = str(input("Input your name with uppercase letter: "))
+# gender = str(input("Input your gender with a F or M: "))
+
+# firstChar = name[0]
+# abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"]
+
+# if gender == "F" and firstChar in abc: 
+#     print("You belongs to A group")
+
+# elif gender == "M" and firstChar not in abc:
+#     print("You belong to A group")
+    
+# else:
+#     print("You belongs to B group")
+
+buleano = True
+one = 0
+
+while buleano == True:
+    if one < 100:
+        one += 1
+        print(one)
+    else:
+        buleano = False
+
+for a in range(1,101):
+    print(a)
+        
+        
