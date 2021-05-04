f5138c4b9564e510114357dce41243b403b37ba6
juanmager <juanmadaria@hotmail.es>
2021-05-04 20:45:06 -0300

    Update

---
+++ Arreglos/10numeros.py
@@ -0,0 +1,8 @@
+# Ejercicio 1
+# Escribir un programa que le pida al usuario que ingrese 10 números enteros (primero uno, luego otro, 
+# y así hasta que el usuario ingrese 10 numeros). 
+# Al final, el programa debe imprimir los números que fueron ingresados en orden inverso, la suma total de los valores y el promedio.
+
+import matplotlib.pyplot as plt
+import numpy as np 
+
